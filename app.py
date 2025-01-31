d5737d0ae974001a372c56f418a358b85355b517
Ari Zerner <ari@zerner.com>
2025-01-30 23:04:03 -0800

    Create downloadable report instead of just displaying advice

--- app.py
+++ app.py
@@ -4,21 +4,43 @@ import markdown
 import asyncio
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
-    advice = None
-    if request.method == 'POST':
-        decklist = request.form['decklist']
-        format = request.form['format']
-        additional_info = request.form['additional_info']
-        
-        # Get the advice
-        advice_text = asyncio.run(mtgai.get_deck_advice(decklist, format=format, additional_info=additional_info))
-        
-        # Convert to HTML
-        advice = markdown.markdown(advice_text)
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def report():
+    decklist = request.form['decklist']
+    format = request.form['format']
+    additional_info = request.form['additional_info']
     
-    return render_template('index.html', advice=advice)
+    advice = asyncio.run(mtgai.get_deck_advice(decklist, format=format, additional_info=additional_info))
+
+    decklist_section = f'''
+## Decklist
+{decklist}
+    '''
+    
+    additional_info_section = f'''
+## Additional Info
+{additional_info}
+    ''' if additional_info else ''
+    
+    format_section = f'''
+## Format
+{format}
+    ''' if format else ''
+    
+    report = f'''# Advice
+{advice}
+
+# Provided Information
+{decklist_section}
+{format_section}
+{additional_info_section}
+    '''.replace('\n', '  \n')
+    
+    return render_template('report.html', content_html=markdown.markdown(report), content_md=report)
 
 if __name__ == '__main__':
     app.run(debug=True)
